a5c6de8f874db98c7ef1df99b3563cac68abca60
Mega Hwee <megahwee@gmail.com>
2022-10-13 12:22:53 +0800

    initial submission

--- temp.py
+++ temp.py
@@ -2,46 +2,21 @@ from typing import List
 
 
 class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        while len(stones) > 2:
-            dic = {}
-            for stone in stones:
-                try:
-                    dic[stone] += 1
-                except:
-                    dic[stone] = 1
-            arr1 = sorted(list(dic.items()))[::-1][0]
-            arr2 = sorted(list(dic.items()))[::-1][1]
-            if arr1[1] == 2:
-                stones = [x for x in stones if x == arr1[0]]
-                continue
-            else:
-                new_stones = []
-                for x in stones:
-                    if x == max(arr1[0], arr2[0]):
-                        new_stones.append(abs(arr1[0]-arr2[0]))
-                        continue
-                    elif x == min(arr1[0], arr2[0]):
-                        continue
-                    else:
-                        new_stones.append(x)
-                stones = new_stones
-        return stones[0]
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        dic = {}
+        for x, y in logs:
+            try:
+                dic[x] = min(y, dic[x])
+            except:
+                dic[x] = y
+        
+        arr = [0 for i in range(k)]
+        for x,y in dic.items():
+            arr[y-1] += 1
+        return arr
 
 
 s = Solution()
-print(s.lastStoneWeight([2, 7, 4, 1, 8, 1]))
-
-
+print(s.findingUsersActiveMinutes(logs = [[0,5],[1,2],[0,2],[0,5],[1,3]], k = 5))
+print(s.findingUsersActiveMinutes(logs = [[1,1],[2,2],[2,3]], k = 4))
 # # Model Solution
-# The idea is based on:
-# (1 ^ 2 ^ 3 ^ .. ^ n) ^ (1 ^ 2 ^ 3 ^ .. ^ n) = 0
-# Suppose we change 'a' to 'b', then all but 'a' and 'b' are XORed exactly 2 times. The result is then
-# 0 ^ a ^ b ^ b ^ b = a ^ b
-# Let c = a ^ b, if we can find 'b' which appears 2 times in the original array, 'a' can be easily calculated by a = c ^ b.
-
-
-# import time
-# for i in range(5):
-#     print(i,end = "\r")
-#     time.sleep(1)
