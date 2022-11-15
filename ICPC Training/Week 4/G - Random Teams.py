483d5f8a55942f4801190b4bbaadf2c36b2b19f9
Mega Hwee <megahwee@gmail.com>
2022-11-15 21:50:55 +0800

    initial submission

--- ICPC Training/Week 4/G - Random Teams.py
+++ ICPC Training/Week 4/G - Random Teams.py
@@ -2,7 +2,7 @@
 # @Author: Ang Jian Hwee
 # @Date:   2022-11-15 20:40:54
 # @Last Modified by:   Ang Jian Hwee
-# @Last Modified time: 2022-11-15 21:43:17
+# @Last Modified time: 2022-11-15 21:49:54
 
 n,m = [int(x) for x in input().strip().split()]
 
@@ -10,6 +10,8 @@ if m == 0:
 	print(0,0)
 elif m == 1:
 	print(int(((m-n)*(m-n-1)/2)), int(((m-n)*(m-n-1)/2)))
+elif n <= m:
+	print(0,0)
 else:
 	p_per_g = n//m
 	n_extra_group = n%m
